ef8990aa6721b0d164a2884e98f8253cd55af6a3
Adam Olsen <arolsen@gmail.com>
2014-03-20 21:45:08 -0600

    fading works

--- miniblinkc3.c
+++ miniblinkc3.c
@@ -21,9 +21,10 @@
 
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/timer.h>
 
 #define PORT_LED GPIOA
-#define PIN_LED GPIO9
+#define PIN_LED GPIO7
 
 static void gpio_setup(void)
 {
@@ -38,39 +39,59 @@ static void gpio_setup(void)
 
 	/* Set GPIO6 (in GPIO port B) to 'output push-pull'. */
 	/* Using API functions: */
-	gpio_mode_setup(PORT_LED, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, PIN_LED);
+    gpio_mode_setup(PORT_LED, GPIO_MODE_AF,
+                        GPIO_OTYPE_PP, PIN_LED);
+    gpio_set_output_options(PORT_LED, GPIO_OTYPE_PP, GPIO_OSPEED_HIGH,
+        PIN_LED);
+    gpio_set_af(GPIOA, GPIO_AF1, GPIO7);
+
+    rcc_periph_clock_enable(RCC_TIM3);                  
+    timer_reset(TIM3);
+    timer_set_mode(TIM3, TIM_CR1_CKD_CK_INT,
+        TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP);
+    int prescaler = (uint16_t) ((48000000 / 2) / 21000000) - 1;
+
+    timer_set_prescaler(TIM3, prescaler);
+    //timer_set_repetition_counter(TIM3, 0);
+    timer_enable_preload(TIM3);
+    timer_continuous_mode(TIM3);
+    timer_set_period(TIM3, 665);
+    timer_disable_oc_output(TIM3, TIM_OC2);
+    timer_set_oc_mode(TIM3, TIM_OC2, TIM_OCM_PWM1);
+    timer_set_oc_value(TIM3, TIM_OC2, 500);
+    timer_set_oc_polarity_high(TIM3, TIM_OC2);
+    timer_enable_oc_output(TIM3, TIM_OC2);
+    timer_enable_preload_complementry_enable_bits(TIM3);
+    timer_enable_counter(TIM3);
 }
 
 int main(void)
 {
-	int i;
+	volatile int i;
+    volatile int brightness = 0;
+    volatile int up = 1;
 
 	gpio_setup();
 
-	/* Blink the LED (PC8) on the board. */
-	while (1) {
-		/* Manually: */
-		// GPIOC_BSRR = PIN_LED;		/* LED off */
-		// for (i = 0; i < 1000000; i++)	/* Wait a bit. */
-		//	__asm__("nop");
-		// GPIOC_BRR = PIN_LED;		/* LED on */
-		// for (i = 0; i < 1000000; i++)	/* Wait a bit. */
-		//	__asm__("nop");
+    /* Infinite loop */
+    while (1)  // Do not exit
+    {
+        if (up) {
+            brightness++;
+        } else {
+            brightness--;
+        }
 
-		/* Using API functions gpio_set()/gpio_clear(): */
-		// gpio_set(PORT_LED, PIN_LED);	/* LED off */
-		// for (i = 0; i < 1000000; i++)	/* Wait a bit. */
-		//	__asm__("nop");
-		// gpio_clear(PORT_LED, PIN_LED);	/* LED on */
-		// for (i = 0; i < 1000000; i++)	/* Wait a bit. */
-		//	__asm__("nop");
+        if (brightness >= 700) {
+            up = 0;
+        } else if (brightness <= 0) {
+            up = 1;
+        }
 
-		/* Using API function gpio_toggle(): */
-		gpio_toggle(PORT_LED, PIN_LED);
-		for (i = 0; i < 1000000; i++) {	
-			__asm__("nop");
-		}
-	}
+        timer_set_oc_value(TIM3, TIM_OC2, brightness);
 
+        for (i = 0; i < 2500; i++)
+            ;  // delay
+    }
 	return 0;
 }
